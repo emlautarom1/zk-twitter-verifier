4cb59c66f4964889185c6db19ff926276ffea229
Lautaro Emanuel <emlautarom1@gmail.com>
2024-01-30 16:31:58 -0300

    Use Field instead of UInt32/64

--- src/BigInt.test.ts
+++ src/BigInt.test.ts
@@ -1,22 +1,22 @@
 import {
   AccountUpdate,
+   Field,
    Mina,
    PrivateKey,
    Provable,
    PublicKey,
    SmartContract,
    Struct,
-   UInt32,
-   UInt64,
    method
 } from "o1js";
 
-const Words64 = Provable.Array(UInt32, 64);
+// We'll use each `Field` as a `UInt32`, meaning that we need 64 `Field`s to represent a `UInt2048`
+const Words64 = Provable.Array(Field, 64);
 
 export class UInt2048 extends Struct({ words: Words64 }) {
 
   static zero() {
-    return new UInt2048({ words: Array(64).fill(UInt32.zero) });
+    return new UInt2048({ words: Array(64).fill(Field.from(0)) });
   }
 
   static fromHexString(hexString: string) {
@@ -32,29 +32,30 @@ export class UInt2048 extends Struct({ words: Words64 }) {
     for (let i = 1; i <= chars.length / wordSize; i++) {
       const offset = chars.length - (i * wordSize);
       const word = chars.slice(offset, offset + wordSize).join("");
-      res.words[i - 1] = UInt32.from(`0x${word}`);
+      res.words[i - 1] = Field.from(`0x${word}`);
     }
     return res;
   }
 
   sub(other: UInt2048): UInt2048 {
     let res = UInt2048.zero();
-    let borrow = UInt64.zero;
+    let borrow = Field.from(0);
     for (let i = 0; i < 64; i++) {
-      let minuend = this.words[i].toUInt64();
-      // Instead of (-1) the minuend in case of borrow, we (+1) the substrahend as UInt64, avoiding underflows
-      let substrahend = other.words[i].toUInt64().add(borrow);
+      let minuend = this.words[i];
+      // Instead of (-1) the minuend in case of borrow, we (+1) the substrahend, avoiding underflows
+      // Overflows can't happen either due to the subtrahend being at most (2^32 - 1)
+      let substrahend = other.words[i].add(borrow);
 
       // In case the minuend is less than the substrahend, we have to borrow from the next word
       // If we borrow, we add 2^32 to the minuend
-      borrow = Provable.if(minuend.lessThan(substrahend), UInt64.one, UInt64.zero);
+      borrow = Provable.if(minuend.lessThan(substrahend), Field.from(1), Field.from(0));
       minuend = Provable.if(
         minuend.lessThan(substrahend),
-        minuend.add(UInt64.from(4294967296 /* 2^32 */)),
+        minuend.add(Field.from(4294967296 /* 2^32 */)),
         minuend);
 
       // Perform the subtraction, where no underflow can happen
-      res.words[i] = minuend.sub(substrahend).toUInt32();
+      res.words[i] = minuend.sub(substrahend);
     }
 
     return res;
@@ -64,19 +65,24 @@ export class UInt2048 extends Struct({ words: Words64 }) {
     let result: UInt2048 = UInt2048.zero();
 
     for (let j = 0; j < 64; j++) {
-      let carry = UInt64.zero;
+      let carry = Field.from(0);
       for (let i = 0; i + j < 64; i++) {
         // Perform the multiplication in UInt64 to ensure that the result always fits (no overflow here)
-        let product: UInt64 = this.words[i].toUInt64()
-          .mul(other.words[j].toUInt64())
+        let product: Field = this.words[i]
+          .mul(other.words[j])
           // Add the previous result for this word index
-          .add(result.words[i + j].toUInt64())
+          .add(result.words[i + j])
           // Lastly, add the previous carry
           .add(carry);
 
-        let { quotient: highBits, rest: lowBits } = product.divMod(UInt64.from(4294967296 /* 2^32 */));
+        // We don't have access to your typicall masking and shifting operations,
+        // so in order to extract the low and high bits we'll use the bit array representation.
+        let bits = product.toBits(64);
+        let lowBits = Field.fromBits(bits.slice(0, 32));
+        let highBits = Field.fromBits(bits.slice(32, 64));
+
         // Keep only the value that fits in a UInt32 (the low bits)
-        result.words[i + j] = lowBits.toUInt32();
+        result.words[i + j] = lowBits;
         // Extract the carry from the product by keeping the bits that could not fit in a UInt32 (the high bits).
         // This carry will be used in the next iteration
         carry = highBits;
@@ -132,7 +138,7 @@ describe("BigInt", () => {
     await txn.sign([deployerKey, zkAppPrivateKey]).send();
   }
 
-  it("substracts", async () => {
+  it.only("substracts", async () => {
     await localDeploy();
 
     let a = UInt2048.fromHexString("0xFFFFFFFFBBBBBBBB");
@@ -145,11 +151,11 @@ describe("BigInt", () => {
     await retrieve.prove();
     await retrieve.sign([user_Key]).send();
 
-    expect(res.words[0].toBigint()).toBe(BigInt("0x11111111"));
-    expect(res.words[1].toBigint()).toBe(BigInt("0x33333333"));
+    expect(res.words[0].toBigInt()).toBe(BigInt("0x11111111"));
+    expect(res.words[1].toBigInt()).toBe(BigInt("0x33333333"));
     for (let i = 2; i < res.words.length; i++) {
       const word = res.words[i];
-      expect(word.toBigint()).toBe(0n);
+      expect(word.toBigInt()).toBe(0n);
     }
   });
 
@@ -166,11 +172,11 @@ describe("BigInt", () => {
     await retrieve.prove();
     await retrieve.sign([user_Key]).send();
 
-    expect(res.words[0].toBigint()).toBe(BigInt("0x11111111"));
-    expect(res.words[1].toBigint()).toBe(BigInt("0xDDDDDDDE"));
+    expect(res.words[0].toBigInt()).toBe(BigInt("0x11111111"));
+    expect(res.words[1].toBigInt()).toBe(BigInt("0xDDDDDDDE"));
     for (let i = 2; i < res.words.length; i++) {
       const word = res.words[i];
-      expect(word.toBigint()).toBe(BigInt("0xFFFFFFFF"));
+      expect(word.toBigInt()).toBe(BigInt("0xFFFFFFFF"));
     }
   });
 
@@ -187,10 +193,10 @@ describe("BigInt", () => {
     await retrieve.prove();
     await retrieve.sign([user_Key]).send();
 
-    expect(res.words[0].toBigint()).toBe(BigInt("0x55555556"));
+    expect(res.words[0].toBigInt()).toBe(BigInt("0x55555556"));
     for (let i = 1; i < res.words.length; i++) {
       const word = res.words[i];
-      expect(word.toBigint()).toBe(BigInt("0xFFFFFFFF"))
+      expect(word.toBigInt()).toBe(BigInt("0xFFFFFFFF"))
     }
   });
 
@@ -209,13 +215,13 @@ describe("BigInt", () => {
     await retrieve.prove();
     await retrieve.sign([user_Key]).send();
 
-    expect(res.words[0].toBigint()).toBe(BigInt("0x2D82D82E"));
-    expect(res.words[1].toBigint()).toBe(BigInt("0xCCCCCCCD"));
-    expect(res.words[2].toBigint()).toBe(BigInt("0x6C16C16A"));
-    expect(res.words[3].toBigint()).toBe(BigInt("0xEEEEEEEE"));
+    expect(res.words[0].toBigInt()).toBe(BigInt("0x2D82D82E"));
+    expect(res.words[1].toBigInt()).toBe(BigInt("0xCCCCCCCD"));
+    expect(res.words[2].toBigInt()).toBe(BigInt("0x6C16C16A"));
+    expect(res.words[3].toBigInt()).toBe(BigInt("0xEEEEEEEE"));
     for (let i = 4; i < res.words.length; i++) {
       const word = res.words[i];
-      expect(word.toBigint()).toBe(0n);
+      expect(word.toBigInt()).toBe(0n);
     }
   });
 });
@@ -227,13 +233,13 @@ describe("BigInt JS", () => {
 
     let res = a.mul(b);
 
-    expect(res.words[0].toBigint()).toBe(BigInt("0x2D82D82E"));
-    expect(res.words[1].toBigint()).toBe(BigInt("0xCCCCCCCD"));
-    expect(res.words[2].toBigint()).toBe(BigInt("0x6C16C16A"));
-    expect(res.words[3].toBigint()).toBe(BigInt("0xEEEEEEEE"));
+    expect(res.words[0].toBigInt()).toBe(BigInt("0x2D82D82E"));
+    expect(res.words[1].toBigInt()).toBe(BigInt("0xCCCCCCCD"));
+    expect(res.words[2].toBigInt()).toBe(BigInt("0x6C16C16A"));
+    expect(res.words[3].toBigInt()).toBe(BigInt("0xEEEEEEEE"));
     for (let i = 4; i < res.words.length; i++) {
       const word = res.words[i];
-      expect(word.toBigint()).toBe(0n);
+      expect(word.toBigInt()).toBe(0n);
     }
   });
 });
